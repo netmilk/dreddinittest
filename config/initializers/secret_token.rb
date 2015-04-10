6e7f58fb5f74d5708f20d370312b93e7c10cfded
Adam Kliment <adam@apiary.io>
2015-04-10 12:52:38 -0700

    Rails bootstrapped, blueprint added, Dredd inited

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dreddinittest::Application.config.secret_key_base = '8ab0ef381d59dd49c5b8cac160cc3f729c78ff5bfb11b2d0941557a913e3ef2cfb88872783b102c855e9964791723f8ef8ad5b0a215b1f265baa7c1fc8bcc9e6'
